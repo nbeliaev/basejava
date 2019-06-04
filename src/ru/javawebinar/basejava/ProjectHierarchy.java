63ee7c298d9fb39a3a899a15f30678ef3fc38104
nikolaibeliaev <fr13.dev@gmail.com>
2019-06-04 11:49:49 +0700

    HW8/feature, add print of project hierarchy

---
+++ src/ru/javawebinar/basejava/ProjectHierarchy.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+
+public class ProjectHierarchy {
+    public static void main(String[] args) {
+        final File parentDirectory = new File("./");
+        final String separator = "";
+        print(parentDirectory, separator);
+    }
+
+    private static void print(final File parentDirectory, final String separator) {
+        if (parentDirectory.isDirectory()) {
+            final File[] files = parentDirectory.listFiles();
+            if (files != null) {
+                for (File file: files) {
+                    System.out.println(separator + file.getName());
+                    if (file.isDirectory()) {
+                        print(file, separator + "\t");
+                    }
+                }
+            }
+        }
+    }
+
+}
