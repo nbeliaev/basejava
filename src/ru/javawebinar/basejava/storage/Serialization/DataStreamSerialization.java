30418b56054b49b7c25d4c7d950bf9d1b121dfaa
Nick <fr13.dev@gmail.com>
2019-07-18 10:51:42 +0300

    HW10/refactoring, class DataStreamSerialization, method doRead

--- src/ru/javawebinar/basejava/storage/Serialization/DataStreamSerialization.java
+++ src/ru/javawebinar/basejava/storage/Serialization/DataStreamSerialization.java
@@ -15,59 +15,8 @@ public class DataStreamSerialization implements SerializationStrategy {
         try (DataOutputStream out = new DataOutputStream(outputStream)) {
             out.writeUTF(resume.getUuid());
             out.writeUTF(resume.getFullName());
-
-            out.writeByte(resume.getContacts().size());
-            final Map<ContactType, String> contacts = resume.getContacts();
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                writeWithException(entry, (action -> {
-                    writeLine(out, entry.getKey().name());
-                    writeLine(out, entry.getValue());
-                }));
-            }
-
-            out.writeByte(resume.getSections().size());
-            final Map<SectionType, Section> sections = resume.getSections();
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                SectionType sectionType = entry.getKey();
-                writeLine(out, sectionType.name());
-                switch (sectionType) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        writeWithException(entry, (action -> writeLine(out, ((SimpleSection) entry.getValue()).getContent())));
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        final List<String> simpleSections = ((ListSection) entry.getValue()).getContent();
-                        out.writeByte(simpleSections.size());
-                        for (String item : simpleSections) {
-                            writeWithException(item, (action -> writeLine(out, item)));
-                        }
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        final List<Organization> organizationSections = ((OrganizationSection) entry.getValue()).getContent();
-                        out.writeByte(organizationSections.size());
-                        for (Organization item : organizationSections) {
-                            writeWithException(item, (action -> {
-                                Link link = item.getLink();
-                                writeLine(out, link.getName());
-                                writeLine(out, link.getUrl());
-                            }));
-
-                            out.writeByte(item.getPositions().size());
-                            final List<Organization.Position> positions = item.getPositions();
-                            for (Organization.Position position : positions) {
-                                writeWithException(position, (action -> {
-                                    writeLine(out, position.getBeginDate().toString());
-                                    writeLine(out, position.getEndDate().toString());
-                                    writeLine(out, position.getTitle());
-                                    writeLine(out, position.getDescription());
-                                }));
-                            }
-                        }
-                        break;
-                }
-            }
+            writeContacts(out, resume);
+            writeSections(out, resume);
         }
     }
 
@@ -76,59 +25,90 @@ public class DataStreamSerialization implements SerializationStrategy {
         try (final DataInputStream input = new DataInputStream(inputStream)) {
             Resume resume = new Resume(readLine(input), readLine(input));
 
-            byte size;
-            size = input.readByte();
-            for (byte i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(readLine(input)), readLine(input));
-            }
+            readContacts(input, resume);
+            readSections(input, resume);
 
-            size = input.readByte();
-            for (byte i = 0; i < size; i++) {
-                SectionType sectionType = SectionType.valueOf(readLine(input));
-                switch (sectionType) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        resume.addSection(sectionType, new SimpleSection(readLine(input)));
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        final byte listSectionSize = input.readByte();
-                        List<String> content = new ArrayList<>();
-                        for (int j = 0; j < listSectionSize; j++) {
-                            content.add(readLine(input));
-                        }
-                        resume.addSection(sectionType, new ListSection(content));
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Organization> organizations = new ArrayList<>();
-                        final byte organizationSectionSize = input.readByte();
-                        for (int j = 0; j < organizationSectionSize; j++) {
-                            final String name = readLine(input);
-                            final String url = readLine(input);
-                            final byte positionSize = input.readByte();
-                            List<Organization.Position> positions = new ArrayList<>();
-                            for (int k = 0; k < positionSize; k++) {
-                                positions.add(
-                                        new Organization.Position(
-                                                LocalDate.parse(input.readUTF()),
-                                                LocalDate.parse(input.readUTF()),
-                                                readLine(input),
-                                                readLine(input)));
-                            }
-                            Organization organization = new Organization(name, url, positions);
-                            organizations.add(organization);
-                        }
-                        resume.addSection(sectionType, new OrganizationSection(organizations));
-                        break;
-                }
-            }
             return resume;
         }
     }
 
+    @FunctionalInterface
+    private interface ActionOnWrite<T> {
+        void writeItem(T item) throws IOException;
+    }
+
+    private <T> void writeWithException(T item, ActionOnWrite<T> action) throws IOException {
+        action.writeItem(item);
+    }
+
     private void writeLine(DataOutputStream out, String str) throws IOException {
         out.writeUTF(str == null ? "" : str);
+    }
+
+    private void writeContacts(DataOutputStream out, Resume resume) throws IOException {
+        final Map<ContactType, String> contacts = resume.getContacts();
+        out.writeByte(contacts.size());
+        for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeWithException(entry, (action -> {
+                writeLine(out, entry.getKey().name());
+                writeLine(out, entry.getValue());
+            }));
+        }
+    }
+
+    private void writeSections(DataOutputStream out, Resume resume) throws IOException {
+        final Map<SectionType, Section> sections = resume.getSections();
+        out.writeByte(sections.size());
+        for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+            SectionType sectionType = entry.getKey();
+            writeLine(out, sectionType.name());
+            switch (sectionType) {
+                case PERSONAL:
+                case OBJECTIVE:
+                    writeWithException(entry, (action -> writeLine(out, ((SimpleSection) entry.getValue()).getContent())));
+                    break;
+                case ACHIEVEMENT:
+                case QUALIFICATIONS:
+                    final List<String> simpleSections = ((ListSection) entry.getValue()).getContent();
+                    out.writeByte(simpleSections.size());
+                    for (String item : simpleSections) {
+                        writeWithException(item, (action -> writeLine(out, item)));
+                    }
+                    break;
+                case EXPERIENCE:
+                case EDUCATION:
+                    final List<Organization> organizationSections = ((OrganizationSection) entry.getValue()).getContent();
+                    out.writeByte(organizationSections.size());
+                    for (Organization item : organizationSections) {
+                        writeWithException(item, (action -> {
+                            Link link = item.getLink();
+                            writeLine(out, link.getName());
+                            writeLine(out, link.getUrl());
+                        }));
+
+                        out.writeByte(item.getPositions().size());
+                        final List<Organization.Position> positions = item.getPositions();
+                        for (Organization.Position position : positions) {
+                            writeWithException(position, (action -> {
+                                writeLine(out, position.getBeginDate().toString());
+                                writeLine(out, position.getEndDate().toString());
+                                writeLine(out, position.getTitle());
+                                writeLine(out, position.getDescription());
+                            }));
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    @FunctionalInterface
+    private interface ActionOnRead<T> {
+        T readItem() throws IOException;
+    }
+
+    private <T> T readWithException(ActionOnRead<T> action) throws IOException {
+        return action.readItem();
     }
 
     private String readLine(DataInputStream input) throws IOException {
@@ -136,12 +116,57 @@ public class DataStreamSerialization implements SerializationStrategy {
         return s.isEmpty() ? null : s;
     }
 
-    private <T> void writeWithException(T item, Action action) throws IOException {
-        action.writeItem(item);
+    private void readContacts(DataInputStream input, Resume resume) throws IOException {
+        byte size;
+        size = readWithException(input::readByte);
+        for (byte i = 0; i < size; i++) {
+            resume.addContact(
+                    readWithException(() -> ContactType.valueOf(readLine(input))),
+                    readWithException(() -> readLine(input)));
+        }
     }
 
-    @FunctionalInterface
-    private interface Action<T> {
-        void writeItem(T item) throws IOException;
+    private void readSections(DataInputStream input, Resume resume) throws IOException {
+        byte size = readWithException(input::readByte);
+        for (byte i = 0; i < size; i++) {
+            SectionType sectionType = readWithException(() -> SectionType.valueOf(readLine(input)));
+            switch (sectionType) {
+                case PERSONAL:
+                case OBJECTIVE:
+                    resume.addSection(sectionType, new SimpleSection(readWithException(() -> readLine(input))));
+                    break;
+                case ACHIEVEMENT:
+                case QUALIFICATIONS:
+                    final byte listSectionSize = readWithException(input::readByte);
+                    List<String> content = new ArrayList<>();
+                    for (int j = 0; j < listSectionSize; j++) {
+                        content.add(readWithException(() -> readLine(input)));
+                    }
+                    resume.addSection(sectionType, new ListSection(content));
+                    break;
+                case EXPERIENCE:
+                case EDUCATION:
+                    List<Organization> organizations = new ArrayList<>();
+                    final byte organizationSectionSize = readWithException(input::readByte);
+                    for (int j = 0; j < organizationSectionSize; j++) {
+                        final String name = readWithException(() -> readLine(input));
+                        final String url = readWithException(() -> readLine(input));
+                        final byte positionSize = readWithException(input::readByte);
+                        List<Organization.Position> positions = new ArrayList<>();
+                        for (int k = 0; k < positionSize; k++) {
+                            positions.add(
+                                    new Organization.Position(
+                                            LocalDate.parse(readWithException(input::readUTF)),
+                                            LocalDate.parse(readWithException(input::readUTF)),
+                                            readWithException(() -> readLine(input)),
+                                            readWithException(() -> readLine(input))));
+                        }
+                        Organization organization = new Organization(name, url, positions);
+                        organizations.add(organization);
+                    }
+                    resume.addSection(sectionType, new OrganizationSection(organizations));
+                    break;
+            }
+        }
     }
 }
