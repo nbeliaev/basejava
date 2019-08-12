a5bff080fcfa8b1799d6be463542739870945c9e
Nick <fr13.dev@gmail.com>
2019-08-12 10:14:32 +0300

    HW15/feature, db structure, add section table

--- config/db_init.sql
+++ config/db_init.sql
@@ -10,10 +10,24 @@ CREATE TABLE contact
     id          SERIAL   NOT NULL
         CONSTRAINT contact_pk PRIMARY KEY,
     resume_uuid CHAR(36) NOT NULL
-        CONSTRAINT contact_resume_uuid_fk references resume on delete cascade,
+        CONSTRAINT contact_resume_uuid_fk REFERENCES resume ON DELETE CASCADE,
     type        TEXT     NOT NULL,
     value       TEXT     NOT NULL
 );
 
-CREATE UNIQUE INDEX contact_resume_uuid_type_id_uindex
+CREATE UNIQUE INDEX contact_resume_uuid_type_index
     ON contact (resume_uuid, type);
+
+CREATE TABLE section
+(
+    id          SERIAL   NOT NULL
+        CONSTRAINT section_pk PRIMARY KEY,
+    resume_uuid CHAR(36) NOT NULL
+        CONSTRAINT section_resume_uuid_fk REFERENCES resume ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL
+);
+
+CREATE UNIQUE INDEX section_resume_uuid_type_index
+    ON section (resume_uuid, type);
+
