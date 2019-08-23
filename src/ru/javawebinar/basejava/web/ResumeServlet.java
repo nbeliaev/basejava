75da6c6cd3231e783c638557e0b72f924cccedee
Nick <fr13.dev@gmail.com>
2019-08-23 10:01:43 +0300

    HW16/feature, ResumeServlet, add jsp implementation

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,17 +1,14 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.SqlStorage;
 import ru.javawebinar.basejava.storage.Storage;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     private final Storage storage = new SqlStorage(
@@ -20,37 +17,8 @@ public class ResumeServlet extends HttpServlet {
             Config.getInstance().getDbPwd());
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setContentType("text/html charset=utf-8");
-        response.getWriter().write(initPage(request.getParameter("uuid")));
-    }
-
-    private String initPage(String uuid) {
-        StringBuilder htmlString = new StringBuilder();
-        htmlString.append("<html'><head><meta charset='utf-8'>");
-        htmlString.append("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">");
-        htmlString.append("</head><body><table class = table>");
-        htmlString.append("<thead><tr><th scope='col'>#</th><th scope='col'>uuid</th><th scope='col'>name</th></tr></thead>");
-        final List<Resume> resumes = getResumes(uuid);
-        for (int i = 0; i < resumes.size(); i++) {
-            htmlString.append("<tr>");
-            htmlString.append("<th scope = 'row'>").append((i + 1)).append("</th>");
-            htmlString.append("<td>").append(resumes.get(i).getUuid()).append("</td>");
-            htmlString.append("<td>").append(resumes.get(i).getFullName()).append("</td>");
-            htmlString.append("</tr>");
-        }
-        htmlString.append("</table>").append("</body>").append("</html>");
-        return htmlString.toString();
-    }
-
-    private List<Resume> getResumes(String uuid) {
-        if (uuid == null) {
-            return storage.getAllSorted();
-        } else {
-            final List<Resume> resumes = new ArrayList<>(1);
-            resumes.add(storage.get(uuid));
-            return resumes;
-        }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
---
+++ web/WEB-INF/jsp/list.jsp
@@ -0,0 +1,44 @@
+<%@ page import="ru.javawebinar.basejava.model.Resume" %>
+<%@ page import="java.util.List" %>
+<%@ page import="ru.javawebinar.basejava.model.ContactType" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Resumes list</title>
+</head>
+<body>
+<section>
+    <table class=table>
+        <thead>
+        <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>name</th>
+            <th scope='col'>e-mail</th>
+            <th scope='col'>edit</th>
+            <th scope='col'>delete</th>
+        </tr>
+        </thead>
+        <%
+            final List<Resume> resumes = (List<Resume>) request.getAttribute("resumes");
+            for (int i = 0; i < resumes.size(); i++) {
+        %>
+        <tr>
+            <th scope='row'><%=i%>
+            </th>
+            <td>
+                <a href="resume?=<%=resumes.get(i).getUuid()%>"><%=resumes.get(i).getFullName()%>
+                </a>
+            </td>
+            <td>
+                <%=resumes.get(i).getContact(ContactType.EMAIL)%>
+            </td>
+        </tr>
+        <%
+            }
+        %>
+    </table>
+</section>
+</body>
+</html>
